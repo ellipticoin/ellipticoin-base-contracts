01ff61f196271215c650473e18f6493b2e3a5caa
Mason Fischer <mason@kissr.co>
2019-04-03 15:58:15 -0400

    Allow for multiple memory keys

--- base_token/src/base_token.rs
+++ base_token/src/base_token.rs
@@ -1,25 +1,38 @@
 use ellipticoin::{error, error::Error, export, get_memory, sender, set_memory, Value};
 
+enum Key {
+    Balances,
+    Approvals,
+}
+
 #[export]
 mod base_token {
     pub fn constructor(initial_supply: u64) {
-        set_memory(sender(), initial_supply);
+        set(Key::Balances, sender(), initial_supply)
     }
 
     pub fn balance_of(address: Vec<u8>) -> u64 {
-        get_memory(address)
+        get(Key::Balances, address)
     }
 
     pub fn transfer(receiver_address: Vec<u8>, amount: u64) -> Result<Value, Error> {
-        let sender_balance: u64 = get_memory(sender());
-        let receiver_balance: u64 = get_memory(receiver_address.clone());
+        let sender_balance: u64 = get(Key::Balances, sender());
+        let receiver_balance: u64 = get(Key::Balances, receiver_address.clone());
         if sender_balance >= amount {
-            set_memory(sender(), sender_balance - amount);
-            set_memory(receiver_address, receiver_balance + amount);
+            set(Key::Balances, sender(), sender_balance - amount);
+            set(Key::Balances, receiver_address, receiver_balance + amount);
             Ok(Value::Null)
         } else {
             Err(error::INSUFFICIENT_FUNDS)
         }
+    }
+
+    fn set(key: Key, address: Vec<u8>, value: u64) {
+        set_memory([vec![key as u8], address].concat(), value);
+    }
+
+    fn get(key: Key, address: Vec<u8>) -> u64 {
+        get_memory([vec![key as u8], address].concat())
     }
 }
 
